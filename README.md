a86f68e59de33e2e130f55201f0117164d97fe5a
Hirofumi Wakasugi <baenej@gmail.com>
2019-11-05 13:14:27 +0900

    Redirect dashboard

--- README.md
+++ README.md
@@ -4,16 +4,29 @@ Killer Pads is a plugin like security pads for "prevention is better than cure".
 It activates the default configuration of security and operational efficiency
 to WordPress websites.
 
-## Admin page customization
+## Features
+
+### Admin page customization
 
 - Remove admin dashboard
 - Add favicon to admin pages (`favicon.ico` or `favicon.png` must be placed in your theme's root directory)
 - Disable autosave functions
 
-## Remove REST routes
+### Remove REST routes
 
 - Remove all routes except ones used by famous plugins and explicitly whitelisted
 
-## Security concerns
+### Security concerns
 
 - Disable XML-RPC
+
+## Configuration
+
+### Remove Dashboard function configuration
+
+When activating this plugin, admin home page is redirected to `/edit.php?post_type=post`.
+If you want to change the path to be redirected, add the following to `wp-config.php`:
+
+```php
+define('KILLER_PADS_ADMIN_HOME_PAGE_PATH', "edit.php?post_type=page")
+```
--- includes/AdminPad.php
+++ includes/AdminPad.php
@@ -4,12 +4,25 @@ namespace KillerPads;
 
 class AdminPad
 {
+    private $redirect_path;
+
+    public function __construct()
+    {
+        if (defined('KILLER_PADS_ADMIN_HOME_PAGE_PATH')) {
+            $this->redirect_path = KILLER_PADS_ADMIN_HOME_PAGE_PATH;
+        } else {
+            $this->redirect_path = "edit.php?post_type=post";
+        }
+    }
+
     public function init()
     {
         add_action('login_head', [$this, 'addFavicon']);
         add_action('admin_head', [$this, 'addFavicon']);
-        add_action('admin_menu', [$this, 'removeDashboard']);
-        add_action('admin_menu', [$this, 'removeComments']);
+        add_action('admin_menu', [$this, 'removeDashboardMenu']);
+        add_action('load-index.php', [$this, 'redirectDashboard']);
+        add_action('login_redirect', [$this, 'redirectAfterLogin']);
+        add_action('admin_menu', [$this, 'removeCommentsMenu']);
 
         add_action('wp_print_scripts', [$this, 'disableAutosave']);
     }
@@ -29,12 +42,22 @@ class AdminPad
         }
     }
 
-    public function removeDashboard()
+    public function removeDashboardMenu()
     {
         remove_menu_page('index.php');
     }
 
-    public function removeComments()
+    public function redirectDashboard()
+    {
+        wp_redirect(admin_url($this->redirect_path));
+    }
+
+    public function redirectAfterLogin()
+    {
+        return admin_url($this->redirect_path);
+    }
+
+    public function removeCommentsMenu()
     {
         remove_menu_page('edit-comments.php');
     }
